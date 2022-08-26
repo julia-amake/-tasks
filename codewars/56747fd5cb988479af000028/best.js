71ec6c5259ca4802fe17621dbda96800c53cf268
Yulia Starchenko <imay@amake.ru>
2022-08-26 12:01:10 +0300

    +best practice

--- codewars/56747fd5cb988479af000028/best.js
+++ codewars/56747fd5cb988479af000028/best.js
@@ -1,3 +1,7 @@
-function getMiddle(s) {
+function getMiddle (s) {
   return s.substr(Math.ceil(s.length / 2 - 1), s.length % 2 === 0 ? 2 : 1);
+}
+
+function getMiddle (s) {
+  return s.slice((s.length - 1) / 2, s.length / 2 + 1);
 }
