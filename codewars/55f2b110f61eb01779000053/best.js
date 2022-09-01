3b9ec6b0e38d3c8743906f2aa4fbaeabb7730a78
Yulia Starchenko <imay@amake.ru>
2022-09-01 22:08:40 +0300

    Best solution 2

--- codewars/55f2b110f61eb01779000053/best.js
+++ codewars/55f2b110f61eb01779000053/best.js
@@ -3,3 +3,9 @@ const GetSum = (a, b) => {
     max = Math.max(a, b);
   return (max - min + 1) * (min + max) / 2;
 };
+
+function GetSum( a,b ) {
+  if (a == b) return a;
+  else if (a < b) return a + GetSum(a+1, b);
+  else return a + GetSum(a-1,b);
+}
