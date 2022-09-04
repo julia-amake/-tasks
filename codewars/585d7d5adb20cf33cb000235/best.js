c7886122fa9e14740bc15e801c8ca2815364324a
Yulia Starchenko <imay@amake.ru>
2022-09-04 13:28:32 +0300

    Best solution +

--- codewars/585d7d5adb20cf33cb000235/best.js
+++ codewars/585d7d5adb20cf33cb000235/best.js
@@ -2,3 +2,7 @@ function findUniq(arr) {
   arr.sort((a,b)=>a-b);
   return arr[0]==arr[1]?arr.pop():arr[0]
 }
+
+function findUniq(arr) {
+  return arr.find(n => arr.indexOf(n) === arr.lastIndexOf(n));
+}
