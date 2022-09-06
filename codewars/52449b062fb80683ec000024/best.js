4aed1e4c5cfebb0029f761cfbd204a095c81440d
Yulia Starchenko <imay@amake.ru>
2022-09-06 23:41:51 +0300

    Best solution

---
+++ codewars/52449b062fb80683ec000024/best.js
@@ -0,0 +1,8 @@
+function generateHashtag (str) {
+  return str.length > 140 || str === '' ? false :
+    '#' + str.split(' ').map(capitalize).join('');
+}
+
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
