1a823d18cf23ed127b2d393f1b3db22ad0470c91
Yulia Starchenko <imay@amake.ru>
2022-09-12 13:20:57 +0300

    Fixed

--- codewars/53b138b3b987275b46000115/mine.js
+++ codewars/53b138b3b987275b46000115/mine.js
@@ -3,7 +3,7 @@ function compareVersions (version1, version2) {
   version2 = version2.split('.');
   const l = Math.max(version1.length, version2.length);
   version1.length = l;
-  version1.length = l;
+  version2.length = l;
 
   for(let i = 0; i < l; i++) {
     if(!version1[i]) version1[i] = 0;
